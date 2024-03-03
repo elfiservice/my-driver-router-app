4119e56d1e93e540dbfacee423b5bdf3e7db216c
Armando Jr <elfiservice@hotmail.com>
2024-03-03 14:57:18 -0300

    Update SettingsPage to set the list of avaleable languages after change the tenant

--- MyDriverRouter.Maui/Pages/SettingsPage.xaml.cs
+++ MyDriverRouter.Maui/Pages/SettingsPage.xaml.cs
@@ -1,9 +1,35 @@
+using MyDriverRouter.CoreBusiness;
+using MyDriverRouter.UseCases;
+
 namespace MyDriverRouter.Maui.Pages;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
+    private readonly IProvideTenantUseCase provideTenantUseCase;
+
+    public SettingsPage(IProvideTenantUseCase provideTenantUseCase)
 	{
 		InitializeComponent();
+		this.provideTenantUseCase = provideTenantUseCase;
 	}
+
+	// void OnTenantEntryCompleted(object sender, EventArgs e)
+	// {
+	// 	DisplayAlert("Alert", "You have been alerted", "OK");
+	// 	string newTenantValue = ((Entry)sender).Text;
+	// 	DisplayAlert("Alert", $"You have been alerted {newTenantValue}", "OK");
+	// 	picker.ItemsSource = this.provideTenantUseCase.ExecuteAsync(newTenantValue).Result as Language[];
+	// }
+	// Completed="OnTenantEntryCompleted"
+
+	async void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+	{
+		// string oldText = e.OldTextValue;
+		// string newText = e.NewTextValue;
+		string newTenantValue = tenantEntry.Text;
+		// DisplayAlert("Alert", $"You have been alerted {newTenantValue}, {newText}, {oldText}", "OK");
+		picker.ItemsSource = await this.provideTenantUseCase.ExecuteAsync(newTenantValue) as Language[];
+	}
+
+	//                TextChanged="OnEntryTextChanged"
 }
