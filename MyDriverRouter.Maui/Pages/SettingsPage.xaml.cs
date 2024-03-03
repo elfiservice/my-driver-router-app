aa0822feceeec2be0e8f3e4ab3da551a911ece3d
Armando Jr <elfiservice@hotmail.com>
2024-03-03 15:10:47 -0300

    Remove commented out code in SettingsPage.xaml.cs

--- MyDriverRouter.Maui/Pages/SettingsPage.xaml.cs
+++ MyDriverRouter.Maui/Pages/SettingsPage.xaml.cs
@@ -13,23 +13,10 @@ public partial class SettingsPage : ContentPage
 		this.provideTenantUseCase = provideTenantUseCase;
 	}
 
-	// void OnTenantEntryCompleted(object sender, EventArgs e)
-	// {
-	// 	DisplayAlert("Alert", "You have been alerted", "OK");
-	// 	string newTenantValue = ((Entry)sender).Text;
-	// 	DisplayAlert("Alert", $"You have been alerted {newTenantValue}", "OK");
-	// 	picker.ItemsSource = this.provideTenantUseCase.ExecuteAsync(newTenantValue).Result as Language[];
-	// }
-	// Completed="OnTenantEntryCompleted"
-
 	async void OnEntryTextChanged(object sender, TextChangedEventArgs e)
 	{
-		// string oldText = e.OldTextValue;
-		// string newText = e.NewTextValue;
 		string newTenantValue = tenantEntry.Text;
 		// DisplayAlert("Alert", $"You have been alerted {newTenantValue}, {newText}, {oldText}", "OK");
 		picker.ItemsSource = await this.provideTenantUseCase.ExecuteAsync(newTenantValue) as Language[];
 	}
-
-	//                TextChanged="OnEntryTextChanged"
 }
