560da17f8ecb01485255dbb95480310fef6a370a
Armando Jr <elfiservice@hotmail.com>
2024-03-25 16:24:44 -0300

    added TRanslation - using LocalizationManager plugin

--- MyDriverRouter.Maui/MauiProgram.cs
+++ MyDriverRouter.Maui/MauiProgram.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using LocalizationResourceManager.Maui;
+using Microsoft.Extensions.Logging;
 using MyDriverRouter.Maui.Pages;
+using MyDriverRouter.Maui.Resources.Languages;
 using MyDriverRouter.Plugins.DataStore.InMemory;
 using MyDriverRouter.UseCases;
 
@@ -16,7 +18,12 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.UseLocalizationResourceManager(settings =>
+			{
+				settings.AddResource(AppResources.ResourceManager);
+				settings.RestoreLatestCulture(true);
+			});;
 
 #if DEBUG
 		builder.Logging.AddDebug();
